a70845616e42e02836d8594d9868436e81745080
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-06-17 18:37:42 -0300

    Empty URL check created

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,6 @@ const navMobile = document.querySelector('.nav__content')
 
 burguer.addEventListener('click', navAccess)
 
-
 function navAccess() {
     if(navMobile.classList.contains('visible')) {
         navMobile.classList.remove('visible')
@@ -12,4 +11,43 @@ function navAccess() {
         navMobile.classList.remove('hidden')
         navMobile.classList.add('visible')
     }
+}
+
+
+const input = document.querySelector('.form-input')
+const submitButton = document.querySelector('.form-button')
+
+submitButton.addEventListener('click', sendLink)
+
+function sendLink() {
+    checkLink()
+    shortening()
+    event.preventDefault()
+}
+
+const errorMessage = document.querySelector('.js-empty-form')
+
+function checkLink() {
+    if (input.value === '') {
+        console.log('Sem link')
+        input.classList.add('js-error-link')
+        errorMessage.innerHTML='Please add a link'
+        return
+    }
+    input.classList.remove('js-error-link')
+    errorMessage.innerHTML=''
+    console.log(input.value)
+}
+
+const apiUrl = 'https://api.shrtco.de/v2/shorten?url='
+
+
+function shortening() {
+    fetch(`${apiUrl}${input.value}`)
+    .then(response => {
+        return response.json()
+    })
+    .then(jsonBody => {
+        console.log(jsonBody)
+    })
 }
